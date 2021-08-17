de68566b7dcf5cc4cba1f0ae9417518d917d8eec
유정연 <yujy0302@gmail.com>
2021-08-17 22:54:05 +0900

    lombok 테스트

--- freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/controller/HelloControllerTest.java
+++ freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/controller/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package com.jojoldu.book.springboot.controller;
+package com.jojoldu.book.springboot.web.controller;
 
 
 import org.junit.jupiter.api.Test;
@@ -7,9 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;;
+
 
 
 //Junit5 이상 extendWidth로 변경됨
@@ -27,6 +32,19 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
         .andExpect(status().isOk())
         .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloReturn() throws Exception{
+        String name ="hello";
+        int amount = 1000;
+
+        mvc.perform(
+            get("/hello/dto").param("name", name)
+                                    .param("amount", String.valueOf(amount)))
+                                    .andExpect(status().isOk())
+                                    .andExpect(jsonPath("$.name", is(name)))
+                                    .andExpect(jsonPath("$.amount", is(amount)));
     }
     
 }
