8c76407a74b3930d8b161527f549ca64cc0dd469
유정연 <yujy0302@gmail.com>
2021-08-19 00:12:40 +0900

    jpa 기본 적용, repository test code 작성 및 쿼리로그 설정

---
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,41 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/** 
+ * Entity 클래스에서는 Setter를 생성하지 않음. 구분을 위해서.
+*/
+
+@Getter
+@NoArgsConstructor      //파라미터가 없는 기본 생성자 = public Posts(){}
+@Entity                 //table과 링크될 클래스입니다! 
+public class Posts {
+
+    @Id                 //PK, generated value로 생성 규칙 설정. Identity는 autoincrement를 위한 설정임.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false) //사용하지 않아도 컬럼과 매칭되지만 추가 옵션을 위해 사용함.
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
---
+++ freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired 
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void getPostWithTitleAndContent(){
+        String title ="title";
+        String content ="content";
+
+        postsRepository.save(Posts.builder()
+                                    .title(title)
+                                    .content(content)
+                                    .author("author")
+                                    .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+    
+}
