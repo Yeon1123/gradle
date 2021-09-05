ce11d74efcb4238dd62be75094349a0c401b66eb
유정연 <jyyu@magis.co.kr>
2021-09-05 20:20:07 +0900

    등록(SAVE) 기능 구현

--- freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -10,20 +10,20 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-/** 
+/**
  * Entity 클래스에서는 Setter를 생성하지 않음. 구분을 위해서.
-*/
+ */
 
 @Getter
-@NoArgsConstructor      //파라미터가 없는 기본 생성자 = public Posts(){}
-@Entity                 //table과 링크될 클래스입니다! 
+@NoArgsConstructor // 파라미터가 없는 기본 생성자 = public Posts(){}
+@Entity // table과 링크될 클래스입니다!
 public class Posts {
-
-    @Id                 //PK, generated value로 생성 규칙 설정. Identity는 autoincrement를 위한 설정임.
+    // DB layer
+    @Id // PK, generated value로 생성 규칙 설정. Identity는 autoincrement를 위한 설정임.
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 500, nullable = false) //사용하지 않아도 컬럼과 매칭되지만 추가 옵션을 위해 사용함.
+    @Column(length = 500, nullable = false) // 사용하지 않아도 컬럼과 매칭되지만 추가 옵션을 위해 사용함.
     private String title;
 
     @Column(columnDefinition = "TEXT", nullable = false)
