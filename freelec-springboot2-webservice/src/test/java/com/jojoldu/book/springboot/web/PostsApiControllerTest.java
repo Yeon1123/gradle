09284b0b2696f7b9e5cabb05fb22bc69094ff9b7
유정연 <jyyu@magis.co.kr>
2021-09-21 19:12:52 +0900

    수정/ 조회기능 추가

--- freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
+++ freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.jojoldu.book.springboot.web;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -33,8 +36,8 @@ public class PostsApiControllerTest {
     @Autowired
     private PostsRepository postsRepository;
 
-    @AfterEach
-    public void tearDown() throws Exception {
+    @AfterEach  
+    public void tearDown() throws Exception {   //@Test가 모두 끝나면 실행되어야 함 -> 테스트때 쓴 값이 남아있으면 안됨!
         postsRepository.deleteAll();
     }
 
@@ -42,8 +45,10 @@ public class PostsApiControllerTest {
     public void Posts_setNewPost() throws Exception {
         String title = "title";
         String content = "content";
+
         PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder().title(title).content(content).author("author")
                 .build();
+
         System.out.println("port: " + port);
         String url = "http://localhost:" + port + "/api/v1/posts";
 
@@ -59,4 +64,38 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getContent()).isEqualTo(content);
 
     }
+    
+    @Test
+    public void Posts_updatePost() throws Exception {
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")   
+                .build());
+
+        Long updateId = savedPosts.getId();
+        String exceptedTitle = "title2";
+        String exceptedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(exceptedTitle)
+                .content(exceptedContent)
+                .build();
+        
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(exceptedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(exceptedContent);
+
+    }
+    
+
 }
