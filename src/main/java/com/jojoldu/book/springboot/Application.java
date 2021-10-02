a158c19da189732113dfa670eebb08a1a4e15cf5
유정연 <yujy0302@gmail.com>
2021-10-02 23:11:13 +0900

    freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java -> src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java#

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,8 +2,9 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+@EnableJpaAuditing //작성일, 수정일 -> '감시' 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
